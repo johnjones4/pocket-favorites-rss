1f2c8844b32bd7f7c9eed9e0a04953e8bf51dd06
John Jones <johnjones4@gmail.com>
2017-01-05 21:25:10 -0500

    forgot ports

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t pocket_favorites_rss ./
-docker run -d --name pocket_favorites_rss --restart always pocket_favorites_rss
+docker run -d --name pocket_favorites_rss --restart always -p 8981:8981 pocket_favorites_rss
