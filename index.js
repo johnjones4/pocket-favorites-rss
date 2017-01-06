803c970900b4a83cebab9efaea94509a94f21838
John Jones <johnjones4@gmail.com>
2017-01-05 21:15:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const config = require('./config');
+const GetPocket = require('node-getpocket');
+const RSS = require('rss');
+
+const redirect = config.urlRoot + '/callback';
+const pocket = new GetPocket({
+  'redirect_uri': redirect,
+  'consumer_key': config.pocket.consumerKey
+});
+
+var accessToken;
+var requestToken;
+
+const app = express();
+
+app.get('/',function(req,res,next) {
+  if (accessToken) {
+    const pocket = new GetPocket({
+      'consumer_key': config.pocket.consumerKey,
+      'access_token': accessToken
+    });
+    const params = {
+      'favorite': 1
+    }
+    pocket.get(params,function(err,resp) {
+      if (err) {
+        next(err);
+      } else if (resp && resp.list) {
+        const rssFeed = new RSS({
+          'title': 'Pocket Favorites',
+          'pubDate': new Date()
+        });
+        for(var itemId in resp.list) {
+          const item = resp.list[itemId];
+          rssFeed.item({
+            'title': item.given_title,
+            'description': item.excerpt,
+            'url': item.given_url,
+            'date': new Date(parseInt(item.time_added) * 1000)
+          });
+        }
+        res.setHeader('Content-type','application/xml');
+        res.send(rssFeed.xml());
+      }
+    });
+  } else {
+    var params = {
+      'redirect_uri': redirect
+    };
+    pocket.getRequestToken(params, function(err, resp, body) {
+      if (err) {
+        next(err);
+      } else {
+        var json = JSON.parse(body);
+        requestToken = json.code;
+        var url = pocket.getAuthorizeURL({
+          'consumer_key': config.pocket.consumerKey,
+          'request_token': requestToken,
+          'redirect_uri': redirect
+        });
+        res.redirect(url);
+      }
+    });
+  }
+});
+
+app.get('/callback',function(req,res,next) {
+  if (requestToken) {
+    var params = {
+      'request_token': requestToken
+    };
+    pocket.getAccessToken(params, function(err, resp, body) {
+      if (err) {
+        next(err);
+      } else {
+        var json = JSON.parse(body);
+        accessToken = json.access_token;
+        res.redirect('/');
+      }
+    });
+  } else {
+    res.send(400);
+  }
+});
+
+app.listen(config.express.port,function() {
+  console.log('Running');
+});
